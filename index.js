62d55b369e55ee6b868125fd93353f4a2372539b
Shadan Rashid <83120023+Rashid-123@users.noreply.github.com>
2024-05-06 12:25:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,26 +11,22 @@ const app = express();
 
 app.use(express.json({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
-// app.use(cors({
-//   credentials: true,
-//   origin: "https://wordwise-31nrylxdb-shadan-rashids-projects.vercel.app"
-// }));
-// app.use(cors());
 
-
+// Allow requests from 'http://localhost:3000' during development
+app.use(cors({
+  credentials: true,
+  origin: "http://localhost:3000"
+}));
 
 app.use(upload());
-//When you use app.use(express.static(__dirname + "/uploads")),
-//you're telling Express to use the express.static middleware
-//for any incoming request to serve static files from the uploads directory.
 app.use("/uploads", express.static(__dirname + "/uploads"));
-//
+
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
-//
+
 app.use(notFound);
 app.use(errorHandler);
+
 connect(process.env.MONGO_URI)
   .then(() => {
     app.listen(process.env.PORT || 5000, () => {
@@ -40,4 +36,3 @@ connect(process.env.MONGO_URI)
   .catch((error) => {
     console.error("Database connection error:", error);
   });
-
