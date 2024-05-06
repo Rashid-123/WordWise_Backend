5ae912d8a70ecf405537ce819e98d961f5644887
Shadan Rashid <83120023+Rashid-123@users.noreply.github.com>
2024-05-06 12:03:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 // // app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(cors({
   credentials: true,
-  origin: 'https://wordwise-31nrylxdb-shadan-rashids-projects.vercel.app'
+  origin: "https://wordwise-31nrylxdb-shadan-rashids-projects.vercel.app"
 }));
 // app.use(cors());
 
